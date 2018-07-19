3f636e04739c522cb06501a722f69baa63f0dd3a
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2018-07-19 16:41:48 -0400

    added all functionality to playlist, users, and session routes. still need to added playlist data

---
+++ public/js/app.js
@@ -0,0 +1,100 @@
+const app = angular.module('MyApp', []);
+
+app.controller('MainController', ['$http', function($http){
+  this.h1 = 'Playlist App'
+  this.playlist = ''
+  this.playlists = []
+
+  this.createForm = {}
+  this.createPlaylist = () => {
+    $http({
+      method: 'POST',
+      url: '/playlists',
+      data: this.createForm
+    }).then(response => {
+      console.log(response.data);
+      this.playlists.unshift(response.data);
+      this.createForm = {}
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  this.getPlaylist = () => {
+    $http({
+      method: 'GET',
+      url: '/playlists',
+    }).then(response => {
+      console.log(response.data);
+      this.playlists = response.data
+      this.playlist = this.playlists[0]
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  this.deletePlaylist = (id) => {
+    $http({
+      method: 'DELETE',
+      url: '/playlist/' + id
+    }).then(response => {
+      console.log(response.data);
+      const removeByIndex = this.playlists.findIndex(playlist => playlist._id === id)
+      this.playlists.splice(removeByIndex, 1)
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  this.updatePlaylist = () => {
+    $http({
+      method: 'PUT',
+      url: '/playlist/' + playlist._id,
+      data: {
+
+      }
+    }).then(response => {
+      console.log(response.data);
+      this.getPlaylist();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  this.createUser = () => {
+    $http({
+      method: 'POST',
+      url: '/users',
+      data: {
+        username: this.username,
+        password: this.password
+      }
+    }).then(response => {
+      console.log(response);
+    })
+  }
+
+  this.logIn = () => {
+    $http({
+      method: 'POST',
+      url: '/sessions',
+      data: {
+        username: this.username,
+        password: this.password
+      }
+    }).then(response => {
+      console.log(response);
+      this.loggedInUser();
+    })
+  }
+
+  this.loggedInUser = () => {
+    $http({
+      method: 'GET',
+      url: '/log'
+    }).then(response => {
+      this.loggedIn = response.data.username
+    })
+  }
+  this.getPlaylist();
+}]);
--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('index')
-});
+// app.get('/', (req, res) => {
+//   res.send('index')
+// });
 
 app.listen(3000, () => {
   console.log('listening......');
